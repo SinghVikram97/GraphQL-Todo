d73c2ec843237091fbcfe3526265a1c1266b7896
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2019-01-31 22:22:26 +0530

    GraphQl Api done

---
+++ todos.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    id: 1,
+    title: "Learn GraphQL",
+    done: false
+  },
+  {
+    id: 2,
+    title: "Learn nanoql",
+    done: false
+  },
+  {
+    id: 3,
+    title: "Learn Schemas",
+    done: false
+  }
+];
